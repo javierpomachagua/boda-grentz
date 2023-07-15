e621c47bde03fc7e59102019971790876bd14a9f
javierpomachagua <javierpp0303@gmail.com>
2023-07-15 17:23:06 -0500

    wip

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -43,8 +44,8 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-//    public function canAccessFilament(): bool
-//    {
-//        return str_ends_with($this->email, '@boda.com');
-//    }
+    public function canAccessFilament(): bool
+    {
+        return $this->email === 'boda@grentz.com';
+    }
 }
