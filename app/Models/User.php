e4f2fb0bee4bdf0dc637fdfc36dc461f12495872
javierpomachagua <javierpp0303@gmail.com>
2023-07-20 19:44:12 -0500

    wip

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,6 +45,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessFilament(): bool
     {
-        return true;
+        return $this->email === 'grentz@boda.com';
     }
 }
